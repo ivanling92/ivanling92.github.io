404c646f54182826c66fb0e1f2f2e727a634942a
Ivan Ling <ivanling92@gmail.com>
2019-12-31 14:33:30 +0800

    Added notes and styling
    Added notes section and received by section. Default to Terrence

--- moissanite/receipt/js/example.js
+++ moissanite/receipt/js/example.js
@@ -18,6 +18,18 @@ function print_today() {
   return today;
 }
 
+function num_gen() {
+  var now = new Date();
+  var months = new Array('01','02','03','04','05','06','07','08','09','10','11','12');
+  var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+  function fourdigits(number) {
+    return (number < 1000) ? number + 1900 : number;
+  }
+  var numr =  (fourdigits(now.getYear()))+ months[now.getMonth()] + date + "XXX";
+  return numr;
+}
+
+
 // from http://www.mediacollege.com/internet/javascript/number/round.html
 function roundNumber(number,decimals) {
   var newString;// The new rounded number
@@ -122,6 +134,7 @@ function passWord() {
 $(document).ready(function() {
 	
 	$("#page-wrap").hide();
+	passWord();
 
 	
 
@@ -151,16 +164,18 @@ $(document).ready(function() {
   $("#delete-logo").click(function(){
     $("#logo").remove();
   });
+  /*
   $("#change-logo").click(function(){
     $("#logo").addClass('edit');
     $("#imageloc").val($("#image").attr('src'));
     $("#image").select();
-  });
+  });*/
   $("#save-logo").click(function(){
     $("#image").attr('src',$("#imageloc").val());
     $("#logo").removeClass('edit');
   });
   
   $("#date").val(print_today());
+  $("#receipt-num").val(num_gen());
   
 });
